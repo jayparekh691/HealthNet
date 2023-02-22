9c8169111e969a370fddd53b8d792a13b2bd590d
Jay Parekh <jay.parekh@iiitb.ac.in>
2023-02-22 21:47:44 +0530

    Login done, Employee deactivate remaining.

--- src/main/java/com/example/project3/repo/EmployeeRepo.java
+++ src/main/java/com/example/project3/repo/EmployeeRepo.java
@@ -9,7 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
     Employee findEmployeeByEmailAndPasswordAndRole(String email,String password,String role);
-
 }
