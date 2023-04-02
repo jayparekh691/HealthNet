d61c95047bad9b154f387ad2a5b03cd2c4b8653a
Jay Parekh <jay.parekh@iiitb.ac.in>
2023-04-02 19:21:52 +0530

    Due FieldWorker done.

---
+++ src/main/java/com/example/project3/entities/VisitModel.java
@@ -0,0 +1,24 @@
+package com.example.project3.entities;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class VisitModel {
+    private int v_id;
+    private String instruction;
+    private String name;
+    private int age;
+    private String address;
+    private String city;
+    private char gender;
+    private String state;
+    private int pincode;
+    private String mobilenumber;
+    private String town;
+    private boolean isvisited;
+    private Date date;
+    private String otp;
+    private int f_id;
+}
--- src/main/java/com/example/project3/services/FieldWorkerServices.java
+++ src/main/java/com/example/project3/services/FieldWorkerServices.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 public interface FieldWorkerServices {
-    List<Appointment> getAppointmentListFW(Integer fid);
+    List<VisitModel> getAppointmentListFW(Integer fid);
     Appointment getVisitDetails(Integer id);
 
-    Visit saveVisit(MedicalData md, Integer f_id);
+    Visit saveVisit(ReceiveVistDataModel md);
 }
